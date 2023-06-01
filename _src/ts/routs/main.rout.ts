9a044657ffe28676ec75f2c8d99d86a69b6f7ed5
GRD <d.grebelny@gmail.com>
2023-06-01 10:38:42 +0400

    GRD README.md

--- _src/ts/routs/main.rout.ts
+++ _src/ts/routs/main.rout.ts
@@ -5,6 +5,16 @@ const mainRout = express.Router();
 
 // root
 mainRout.get('/', (req, res) => {
+  res.send(
+    `<main style="text-align: center; margin: 30vh 0;"><h1>Главная страница приложения Balance-rating</h1>
+        <br>чтобы получить адрес, баланс которого изменился больше остальных используйте запрос: http://localhost:3000/max-balance-acc
+        <br>Источник данных ограничивает количество входящих запросов в мин. время выполнения запроса ~25сек. Наберитесь терпения
+      </main>`,
+  );
+});
+
+// root
+mainRout.get('/max-balance-acc', (req, res) => {
   const ctr = new MainController();
   ctr.getMaxChangedAccount(req, res);
 });
