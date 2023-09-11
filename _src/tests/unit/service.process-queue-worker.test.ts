473cad1f50322f9b96983941a6653cc2230841dc
GRD <d.grebelny@gmail.com>
2023-09-11 23:40:16 +0400

    GRD _src/tests/unit/service.process-queue-worker.test.ts

--- _src/tests/unit/service.process-queue-worker.test.ts
+++ _src/tests/unit/service.process-queue-worker.test.ts
@@ -1,16 +1,19 @@
+// eslint-disable-next-line max-classes-per-file
 import { Service } from '../../services/service';
 import { Account, ProcessWorkerArgs } from '../../models/max-balance.model';
 import errorHandler from '../../errors/handler.error';
 errorHandler.setErrorListener();
 
 describe('processQueueWorker function', () => {
-  let service: Service;
+  class ExtendedService extends Service {
+    sessionKey = 12345;
+    getMaxAccount = jest.fn(() => ({ address1: 10 }));
+  }
+  let extendedService: ExtendedService;
   let args: ProcessWorkerArgs;
 
   beforeEach(() => {
-    service = new Service();
-    service.sessionKey = 12345;
-    service.getMaxAccount = jest.fn(() => ({ address1: 10 }));
+    extendedService = new ExtendedService();
     args = {
       taskNumber: 1,
       blockNumberHex: '0x4e3b7',
@@ -33,43 +36,39 @@ describe('processQueueWorker function', () => {
   });
 
   it('should process a block with transactions', async () => {
-    await service.processQueueWorker(args);
+    await extendedService.processQueueWorker(args);
 
     expect(args.taskCallback).toHaveBeenCalledWith(null);
-    expect(service.addressBalances).toEqual({
+    expect(extendedService.addressBalances).toEqual({
       address1: 10,
       address2: -5,
       address3: -5,
     });
-    expect(service.amountOfTransactions).toBe(2);
+    expect(extendedService.amountOfTransactions).toBe(2);
     expect({ address1: 10 }).toEqual({ address1: 10 });
   });
 
   it('should handle empty content', async () => {
     args.content = null;
-    await service.processQueueWorker(args);
+    await extendedService.processQueueWorker(args);
 
-    expect(service.addressBalances).toEqual(undefined);
-    expect(service.amountOfTransactions).toBe(0);
-    expect(service.maxAccount).toEqual(undefined);
+    expect(extendedService.addressBalances).toEqual(undefined);
+    expect(extendedService.amountOfTransactions).toBe(0);
+    expect(extendedService.maxAccount).toEqual(undefined);
   });
 
   it('should handle exceptions', async () => {
     const mockError = new Error('Test error');
-    class ExtendedService extends Service {
+    class RetryExtendedService extends Service {
       sessionKey = 12345;
-
       getMaxAccount(...ar: Account[]): Account {
         throw mockError;
       }
     }
-    const extendedService = new ExtendedService();
-    await extendedService.processQueueWorker(args);
+    const retryExtendedService = new RetryExtendedService();
+    await retryExtendedService.processQueueWorker(args);
 
     expect(args.taskCallback).toHaveBeenCalledWith(mockError);
     expect(args.reject).toHaveBeenCalledWith(mockError);
-    expect(service.addressBalances).toEqual(undefined);
-    expect(service.amountOfTransactions).toBe(0);
-    expect(service.maxAccount).toBe(undefined);
   });
 });
