6b03ca753af21196cbae4b686c5a7693763bab79
GRD <d.grebelny@gmail.com>
2023-09-12 21:25:43 +0400

    GRD _src/tests/unit/services/service/service.get-max-changed-account.test.ts

---
+++ _src/tests/unit/services/service/service.get-max-changed-account.test.ts
@@ -0,0 +1,63 @@
+import { Service } from '../../../../services/service';
+
+describe('getMaxChangedAccount function', () => {
+  let service: Service;
+
+  beforeEach(() => {
+    service = new Service();
+  });
+
+  it('when balances are positive', () => {
+    const account1 = { address1: 100 };
+    const account2 = { address2: 200 };
+    const result = service.getMaxChangedAccount(account1, account2);
+    expect(result).toEqual(account2);
+  });
+
+  it('when balances are negative', () => {
+    const account1 = { address1: -100 };
+    const account2 = { address2: -200 };
+    const result = service.getMaxChangedAccount(account1, account2);
+    expect(result).toEqual(account2);
+  });
+
+  it('when balances are a mix of positive and negative', () => {
+    const account1 = { address1: 100 };
+    const account2 = { address2: -200 };
+    const result = service.getMaxChangedAccount(account1, account2);
+    expect(result).toEqual(account2);
+  });
+
+  it('when one balance is zero', () => {
+    const account1 = { address1: 0 };
+    const account2 = { address2: 200 };
+    const result = service.getMaxChangedAccount(account1, account2);
+    expect(result).toEqual(account2);
+  });
+
+  it('when both balances are zero', () => {
+    const account1 = { address1: 0 };
+    const account2 = { address2: 0 };
+    const result = service.getMaxChangedAccount(account1, account2);
+    expect(result).toEqual(account1);
+  });
+
+  it('when one balance is NaN', () => {
+    const account1 = { address1: NaN };
+    const account2 = { address2: 200 };
+    const result = service.getMaxChangedAccount(account1, account2);
+    expect(result).toEqual(account2);
+  });
+
+  it('when both balances are NaN', () => {
+    const account1 = { address1: NaN };
+    const account2 = { address2: NaN };
+    const result = service.getMaxChangedAccount(account1, account2);
+    expect(result).toEqual(account1);
+  });
+
+  it('should return an empty object when no accounts are provided', () => {
+    const result = service.getMaxChangedAccount();
+    expect(result).toEqual(undefined);
+  });
+});
